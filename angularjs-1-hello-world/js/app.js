c252cd83ac3c06ff526d8fb7849d1682c691ce9f
Rodion Bykov <r.bykov@webellian.com>
2015-06-05 15:21:18 +0200

    Finished first chapter (basic directives ng-repeat ng-show ng-hide ng-click)

--- angularjs-1-hello-world/js/app.js
+++ angularjs-1-hello-world/js/app.js
@@ -6,11 +6,22 @@ angular.module('testApp', [])
     self.message = "Hello, ";
     
     self.notes = [
-        {id: 1, label: 'First Note', done: false},
+        {id: 1, label: 'First Note', done: false, assignee: 'Shyam'},
         {id: 2, label: 'Second Note', done: false},
         {id: 3, label: 'Done Note', done: true},
-        {id: 4, label: 'Last Note', done: false}
+        {id: 4, label: 'Last Note', done: false, assignee: 'Brad'}
     ];
+    
+    self.getNoteClass = function(status) {
+        return {
+            done: status,
+            pending: !status
+        };
+    };
+    
+    self.doneNote = function(note){
+        note.done = true;
+    }
     
     console.log('main controller started');
 }]);
