3b086b4be0fcf460d64c17364546c2478d55d18d
Rodion Bykov <rodionbykov@gmail.com>
2016-02-01 21:36:15 +0100

    Added milesToMeters

--- angularjs-2-unitcalculator/src/app/imperial/imperial.controller.js
+++ angularjs-2-unitcalculator/src/app/imperial/imperial.controller.js
@@ -9,6 +9,20 @@
   function ImperialController() {
     var vm = this;
     vm.name = 'Imperial System';
+    vm.inputValue = 0;
+    vm.milesToMeters = milesToMeters;
+  }
+
+  function inchesToMeters(inches){
+    return inches * 0.0254;
+  }
+
+  function feetToMeters(feet){
+    return feet * 0.3048;
+  }
+
+  function milesToMeters(miles){
+    return miles * 1609.344;
   }
 
 })();
