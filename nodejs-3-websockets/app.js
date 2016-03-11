2730afa74979447817d992f4eb1e7f29d019d59e
Rodion Bykov <rodionbykov@gmail.com>
2016-03-11 15:02:51 +0100

    Added nickname support

--- nodejs-3-websockets/app.js
+++ nodejs-3-websockets/app.js
@@ -7,21 +7,26 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  
-  io.emit('incoming_message', '* new user connected');  
-  console.log('a user connected');
-    
-  socket.on('incoming_message', function(msg){
-    socket.broadcast.emit('incoming_message', msg);
-    console.log('message: ' + msg);
+
+  socket.on("join", function(nickName){
+    socket.nickName = nickName;
+    io.emit("incoming_message", nickName + " connected");
+    console.log(nickName + " connected");
+  })
+
+  socket.on("incoming_message", function(msg){
+    var nickName = socket.nickName;
+
+    socket.broadcast.emit("incoming_message", nickName + ": " + msg);
+    console.log(nickName + ": " + msg);
   });
-    
-  socket.on('disconnect', function(){
-    io.emit('incoming_message', '* user disconnected');  
-    console.log('user disconnected');      
-  });    
+
+  socket.on("disconnect", function(){
+    io.emit('incoming_message', socket.nickName + " disconnected");
+    console.log(socket.nickName + " disconnected");
+  });
 });
-// TODO add nicknames support
+
 http.listen(9090, function(){
   console.log('listening on *:9090');
 });
