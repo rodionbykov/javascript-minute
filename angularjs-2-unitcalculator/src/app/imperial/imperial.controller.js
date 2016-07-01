7253fb3a096f3f55d50ec4e5069d7515093ecba7
Rodion Bykov <rodionbykov@gmail.com>
2016-07-01 17:33:48 +0200

    Disengage converters

--- angularjs-2-unitcalculator/src/app/imperial/imperial.controller.js
+++ angularjs-2-unitcalculator/src/app/imperial/imperial.controller.js
@@ -3,16 +3,16 @@
 
   angular
     .module('unitcalculator')
-    .controller('ImperialController', ['imperialConverter', ImperialController]);
+    .controller('ImperialController', ['ImperialConverter', ImperialController]);
 
   /** @ngInject */
-  function ImperialController(imperialConverter) {
+  function ImperialController(ImperialConverter) {
     var vm = this;
     vm.name = 'Imperial System';
     vm.inputValue = 0;
     vm.inputUnit = 'miles';
     vm.convert = function(toUnit){
-        return imperialConverter.convert(vm.inputValue, vm.inputUnit, toUnit);
+        return ImperialConverter.convert(vm.inputValue, vm.inputUnit, toUnit);
     };
   }
 
